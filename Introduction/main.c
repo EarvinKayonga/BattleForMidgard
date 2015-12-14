94652abe43838528a2082724cece40229432cdce
EarvinKayonga <demoneartin@gmail.com>
2015-12-14 15:58:41 +0100

    first main

---
+++ Introduction/main.c
@@ -0,0 +1,13 @@
+/*
+** main.c for  in /home/earvin/Lab/BattleForMidgard/Introduction
+** 
+** Made by KAYONGA Earvin
+** Login   <kayong_e@etna-alternance.net>
+** 
+** Started on  Mon Dec 14 15:56:08 2015 KAYONGA Earvin
+** Last update Mon Dec 14 15:58:10 2015 KAYONGA Earvin
+*/
+int	main(int argc, char **argv)
+{
+  return (0);
+}
