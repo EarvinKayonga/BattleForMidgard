684b681e72e491a6b81ced503bc83fb881446916
EarvinKayonga <demoneartin@gmail.com>
2015-12-14 18:09:08 +0100

    Boucle

--- Introduction/setting.c
+++ Introduction/setting.c
@@ -5,7 +5,7 @@
 ** Login   <kayong_e@etna-alternance.net>
 ** 
 ** Started on  Mon Dec 14 17:21:45 2015 KAYONGA Earvin
-** Last update Mon Dec 14 17:54:15 2015 KAYONGA Earvin
+** Last update Mon Dec 14 18:08:51 2015 KAYONGA Earvin
 */
 
 #include	"utils.h"
@@ -15,11 +15,17 @@ char		*getArgs()
 {
   char *a;
 
-  my_putstr("Votre Tour (quit pour quitter) > ");
-  if ((a = readLine()) != NULL)
+  my_putstr("Votre Tour (Quit pour quitter) > ");
+  if ((a = readLine()) != NULL &&
+      (!my_strcmp(a, "help me !!!")
+       || !my_strcmp(a, "magic catch")))
     return a;
-  else
+  else if (!my_strcmp(a, "Quit"))
+    return a;
+  else{
+    my_putstrN("Pas compris: help me !!! ou magic catch");
     return getArgs();
+  }
 }
 
 void		setPlayer(char *name)
@@ -29,6 +35,6 @@ void		setPlayer(char *name)
   my_putstr("Bienvenue ");
   my_putstrN(name);
   arg = getArgs();
-  while (my_strcmp(arg, "quit"))
+  while (my_strcmp(arg, "Quit"))
     arg = getArgs();
 }
