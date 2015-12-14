96e64ccbd6422a22d89ad0048a186d4b56aff8ae
EarvinKayonga <demoneartin@gmail.com>
2015-12-14 17:10:21 +0100

    putstrN

---
+++ Introduction/my_putstrN.c
@@ -0,0 +1,17 @@
+/*
+** my_putstrN.c for  in /home/earvin/Lab/BattleForMidgard/Introduction
+** 
+** Made by KAYONGA Earvin
+** Login   <kayong_e@etna-alternance.net>
+** 
+** Started on  Mon Dec 14 17:02:01 2015 KAYONGA Earvin
+** Last update Mon Dec 14 17:07:15 2015 KAYONGA Earvin
+*/
+
+#include	"utils.h"
+
+void		my_putstrN(char *str)
+{
+  my_putstr(str);
+  my_putchar('\n');
+}
--- Introduction/utils.h
+++ Introduction/utils.h
@@ -5,13 +5,16 @@
 ** Login   <kayong_e@etna-alternance.net>
 ** 
 ** Started on  Mon Dec 14 16:10:43 2015 KAYONGA Earvin
-** Last update Mon Dec 14 16:37:53 2015 KAYONGA Earvin
+** Last update Mon Dec 14 17:07:39 2015 KAYONGA Earvin
 */
 #ifndef		UTILS_H
 #define		UTILS_H
 
 char		*readLine();
 int		my_strlen(char *str);
+int		my_strcmp(char *s1, char *s2);
 void		my_putstr(char *str);
+void		my_putchar(char chr);
+void		my_putstrN(char *str);
 
 #endif  /* !UTILS_H */
