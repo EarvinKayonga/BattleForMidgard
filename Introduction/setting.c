f1870f4913e2a9dff5e13d1218b3bb49476420ac
EarvinKayonga <demoneartin@gmail.com>
2015-12-14 17:28:31 +0100

    before

---
+++ Introduction/setting.c
@@ -0,0 +1,16 @@
+/*
+** setting.c for  in /home/earvin/Lab/BattleForMidgard/Introduction
+** 
+** Made by KAYONGA Earvin
+** Login   <kayong_e@etna-alternance.net>
+** 
+** Started on  Mon Dec 14 17:21:45 2015 KAYONGA Earvin
+** Last update Mon Dec 14 17:23:22 2015 KAYONGA Earvin
+*/
+
+#include	"utils.h"
+
+void		setPlayer(char *name)
+{
+  my_putstr(name);
+}
