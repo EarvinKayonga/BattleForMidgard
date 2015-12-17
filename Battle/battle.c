923e8588af88af22cdbc271da314035677159713
stevy97 <dubo_s@etna-alternance.net>
2015-12-17 12:28:30 +0100

    ajout de battle

--- Battle/battle.c
+++ Battle/battle.c
@@ -5,7 +5,7 @@
 ** Login   <dubo_s@etna-alternance.net>
 ** 
 ** Started on  Tue Dec 15 13:51:52 2015 DUBO Stévy
-** Last update Thu Dec 17 09:57:17 2015 DUBO Stévy
+** Last update Thu Dec 17 12:28:09 2015 DUBO Stévy
 */
 
 #include<stdlib.h>
@@ -18,75 +18,89 @@ void		my_put_nbr(int n);
 t_creature      *getCreature();
 char		*readline();
 void		debut(t_player *current, t_creature *monster);
-
-void		aff_list(t_creature *monster)
-{
-  monster = getCreature();
-  my_putstr(monster->name);
-  my_put_nbr(monster->lvl);
-  my_put_nbr(monster->pv);
-  my_put_nbr(monster->pvmax);
-  my_put_nbr(monster->pm);
-  my_put_nbr(monster->pmmax);
-}
+void		aff_team(t_list *list);
+t_list		*add_crearure(t_list *list, t_creature *monster);
 
 void		Outofbattle(t_creature *monster)
 {
   char		*str;
-  t_list	*list;
-  t_player	*player;
-  
-  my_putstr("Votre tour> ");    
+  t_list	*team;
+
   str = readline();
-  if (list != NULL)
-    if (my_strcmp(str, "team") == 0)
-      {
-	my_putstr(str);
-	aff_list(monster);
-      }
-    else if (my_strcmp(str, "you are the chosen one"))
+  monster = getCreature();
+  team = malloc(sizeof(t_list));
+  my_putstr("Votre tour> \n");    
+  if (my_strcmp(str, "team") == 0)
     {
       my_putstr(str);
-      list->monster = getCreature();
+      team = add_creature(team, monster);
+      aff_team(team);
     }
-    else if (my_strcmp(str, "let's fight"))
+  else if (my_strcmp(str, "you are the chosen one\n") == 0)
     {
       my_putstr(str);
-      debut(player, monster);
+      team->monster = getCreature();
     }
   else
-    my_putstr("Vous n'avez pas rentrez de commande valide");
+    my_putstr("Vous n'avez pas rentrez de commande valide \n");
+}
+
+void		beginfight(t_list *team, t_creature *opponent)
+{
+  char		*name;
+  char		*str;
+  t_player	*tmp;
+
+  str = readline();
+  tmp = malloc(sizeof(t_player));
+  team = malloc(sizeof(t_list));
+  name = malloc(sizeof(char));
+  tmp->name = name;
+  tmp->team = team;
+  if (my_strcmp(str, "let's fight\n") == 0)
+    {
+      my_putstr(str);
+      opponent = getCreature();
+      my_putstr(opponent->name);
+      team->monster = getCreature();
+      my_putstr(team->monster->name);
+    }
+  free(tmp);
+  free(team);
+  free(name);
 }
 
 void		attack(t_creature *monster)
 {
+  int		proba;
   char		*s;
   t_creature	*opponent;
 
+  opponent = malloc(sizeof(t_creature));
   s = readline();
-  if (my_strcmp(s, "slash"))
+  proba = rand() % 2;
+  if (my_strcmp(s, "slash\n") == 0)
     {
       my_putstr(s);
       opponent->pv = opponent->pv - 15;
       monster->pm = monster->pm - 3;
     }
-  else if (my_strcmp(s, "fire"))
+  else if (my_strcmp(s, "fire\n") == 0)
     {
       my_putstr(s);
       opponent->pv = opponent->pv - 30;
       monster->pm = monster->pm - 7;
     }
-  else if (my_strcmp(s, "gamble"))
+  else if (my_strcmp(s, "gamble\n") == 0 && proba < 2)
     {
       my_putstr(s);
-      opponent->pv = rand() % 21;
-      monster->pv = rand() % 21;
+      opponent->pv = rand() % 20;
+      monster->pv = rand() % 20;
     }
+  free(opponent);
 }
-void		Inbattle(t_creature *monster)
+/*void		Inbattle(t_creature *monster)
 {
   t_creature	*opponent;
-
-  monster = getCreature();
-  opponent = getCreature();
 }
+*/
